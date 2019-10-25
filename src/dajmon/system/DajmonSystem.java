931a3653e30973957e31234257a97fc0f004baee
Andres Jurado <anmijurane@gmail.com>
2019-10-25 11:19:24 -0500

    Correccion de error con JDK 8

--- src/dajmon/system/DajmonSystem.java
+++ src/dajmon/system/DajmonSystem.java
@@ -3,7 +3,7 @@ package dajmon.system;
 public class DajmonSystem {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
